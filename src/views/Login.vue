520a6808d69ef3241dd0758bd4c7b5aef6606482
wang-qing-yuan <1950016253@qq.com>
2020-02-14 21:48:49 +0800

    前端6.0

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -9,44 +9,108 @@
 			</div>
 
 			<div class="tab-box" v-if="show && selected === 0">
-				<div class="t-center">
-					<input type="text" claplaceholder="请输入手机号" v-model="userDto.mobile" />
-					<input type="password" placeholder="请输入6-16位密码" minlength="6" maxlength="16" v-model="userDto.password" />
+				<form class="drop-down-box" action="">
+					<div class="w-fx-left">
+						<select class="drop-down">
+							<option class="aa">中国+86</option>
+							<option>美国+1</option>
+							<option class="aa">日本+81</option>
+							<option>中国香港+852</option>
+							<option>中国澳门+853</option>
+							<option>中国台湾+886</option>
+							<option class="aa">马来西亚+60</option>
+							<option>澳大利亚+61</option>
+							<option>加拿大+1</option>
+							<option>英国+44</option>
+							<option>新加坡+65</option>
+							<option>德国+49</option>
+						</select>
+						<div class="pho"><input type="text" class="num" placeholder="手机号" /></div>
+					</div>
+					
+				</form>
+				<div class="line"></div>
+				<div class="paw row">
+	<input type="password" class="paw-b" placeholder="请输入6位短信验证码" minlength="6" maxlength="6"  />
+	<p class="paw-note">获取短信验证码</p></div>
+			<div class="w-fx-right">
+					<p class="note-2">接受语音消息</p>
 				</div>
-		
-				<!-- <div class="code-box">
-					<input type="text" placeholder="请输入验证码" v-model="userDto.code" />
-					<div class="img"><img ref="codeImg" @click="refresh()" style="cursor: pointer;" /></div>
-				</div> -->
-			<!-- 	<input type="submit" class="login-btn" value="登录" @click="signIn(userDto)" />
-				<span class="span-tab" @click="changeTab">没有账号，立即前往注册</span> -->
 			</div>
-
-			<!-- <div class="tab-box" v-if="show && selected === 1">
-				<input type="text" placeholder="请设置用户名" v-model="userDto.nickname" />
-				<!-- <input type="tel" placeholder="生日" minlength="11" maxlength="11" v-model="userDto.getBirthday"/> -->
-			<!-- <input type="tel" placeholder="请绑定手机号" minlength="11" maxlength="11" v-model="userDto.mobile" :disabled="yzmDisabled" @input.prevent="checkLength" />
-				<input type="password" placeholder="请设置6-16位密码" minlength="6" maxlength="16" v-model="userDto.password" />
-				<input type="password" placeholder="请确认密码" minlength="6" maxlength="16" v-model="pwd2" />
-		
-				<input type="submit" class="regist-btn" value="注册" @click="signUp(userDto)" />
-			</div> -->
-
-			<!-- <div class="line-box" v-if="show && selected === 0">
-				<span class="line"></span>
+			
+<div class="tab-box" v-if="show && selected === 1">
+				<input type="text" class="num-2" placeholder="手机号或邮箱" v-model="mobile"/>
+				<input type="password" class="num-2" placeholder="密码" v-model="password"/>
+				<div class="w-fx-between forget">
+					<div class="w-fx-left forget-a">
+						<p>邮箱账号登录</p>
+					</div>
+					<div class="w-fx-right forget-b">
+						<p>忘记密码？</p>
+					</div>
+				</div>
+			</div>
+				<div class="login-buttom">
+					<input type="submit" class="login-btn" value="注册/登录" @click="signIn(mobile,password)" />
+					</div>
+			</div>
+			
+			
+			
+			<div class="t-center">
+				<div class="d-inline-block grey-box">
+					<div class="row">
+						<div class="grey-a">
+						<p>为注册手机验证后自动登录</p>
+						<p>注册即代表同意《知乎协议》《隐私保护指引》</p>
+					</div>
+					
+						<div class="w-fx-right grey-b">
+						<p>注册机构号</p>
+					</div></div>
+					
+					
+				</div>
+				</div>
 				
-				<span class="line"></span>
-			</div> -->
+				<div class="t-center">
+					<div class="white-box d-inline-block">
+						<div class="row">
+							<div class="whiter-a">社交账号登录</div>
+						<div class="w-fx-right whiter-b">
+							<svg viewBox="0 0 20 19" class="Icon Login-socialIcon Icon--wechat" width="18" height="18" aria-hidden="true" style="fill: rgb(96, 200, 77); height: 18px; width: 18px;"><title></title><g><path fill-rule="evenodd" d="M.224 18.667s4.24-1.825 4.788-2.056C13.03 20.14 20 14.715 20 8.9 20 3.984 15.523 0 10 0S0 3.984 0 8.898c0 1.86.64 3.585 1.737 5.013-.274.834-1.513 4.757-1.513 4.757zM6.167 8.96c.69 0 1.25-.57 1.25-1.27 0-.703-.56-1.272-1.25-1.272s-1.25.57-1.25 1.27c0 .703.56 1.272 1.25 1.272zm7.583 0c.69 0 1.25-.57 1.25-1.27 0-.703-.56-1.272-1.25-1.272s-1.25.57-1.25 1.27c0 .703.56 1.272 1.25 1.272z"></path></g></svg>
+							<p>微信</p>
+							<svg width="18" height="18" viewBox="0 0 18 20" xmlns="http://www.w3.org/2000/svg" class="Icon Login-socialIcon Icon--qq" aria-hidden="true" style="fill: rgb(80, 200, 253); height: 18px; width: 18px;"><title></title><g><title>QQ</title><path d="M9.003 0c-2.265 0-6.29 1.364-6.29 7.325V8.52S.55 12.96.55 15.474c0 .665.17 1.025.28 1.025.115 0 .903-.485 1.75-2.073 0 0-.18 2.197 1.903 3.967 0 0-1.77.495-1.77 1.182 0 .686 4.078.43 6.29 0 2.24.425 6.287.687 6.287 0 0-.688-1.768-1.182-1.768-1.182 2.085-1.77 1.905-3.967 1.905-3.967.845 1.588 1.634 2.072 1.746 2.072.11 0 .283-.36.283-1.026 0-2.514-2.166-6.954-2.166-6.954V7.325C15.29 1.365 11.268 0 9.003 0z" fill-rule="evenodd"></path></g></svg>
+							<p>QQ</p>
+							<svg viewBox="0 0 22 18" class="Icon Login-socialIcon Icon--weibo" width="18" height="18" aria-hidden="true" style="fill: rgb(251, 102, 34); height: 18px; width: 18px;"><title></title><g><g fill-rule="evenodd">     <path d="M14.518.06s-.87.644.03 1.71c0 0 6.287-1.19 5.69 6.33 0 0 1.05 1.13 1.674-.31 0 .002 1.44-8.584-7.394-7.73zM4.883 13.17s.038 2.584 3.326 2.584c3.657 0 3.683-2.98 3.683-2.98S12.1 9.67 8.687 9.61c-3.863-.07-3.804 3.56-3.804 3.56zM7.41 14.21c-.668 0-1.214-.447-1.214-.998 0-.55.543-.998 1.215-.998.67 0 1.213.447 1.213.998 0 .55-.54.998-1.212.998z"></path>     <path d="M4.317 4.52C-2.603 10.353.873 14.85.873 14.85c.57 1.01 3.382 3.1 8.596 3.1 5.21 0 9.314-3.628 9.314-6.44 0-2.813-2.918-2.714-2.918-2.714 1.04-1.554.19-2.65.19-2.65-1.684-2.118-5.404-.16-5.407-.158.772-1.717.11-2.797.11-2.797C8.932.66 4.317 4.52 4.317 4.52zm10.448 7.79s-.467 4.16-6.447 4.16c-5.745 0-5.82-3.322-5.842-3.712 0 0-.073-4.423 6.58-4.654 5.94-.204 5.71 4.207 5.71 4.207zM18.65 7.045s1.018-4.37-3.864-3.818c0 0-.628.58.09 1.346 0 0 2.602-.58 2.397 2.598 0 0 .715.885 1.376-.125z"></path>   </g></g></svg>
+							<p>微博</p>
+						</div></div>
+						
+					
+				</div></div>
+				
+				<div class="t-center">
+					<div class="d-inline-block white-box-2">
+						<div class="t-center row">
+							<svg width="20" height="20" fill="currentColor" style="position: relative;top: 10px;margin-right: 10px;margin-left: 130px;"><g fill="none" fill-rule="evenodd"><path fill="#0084FF" d="M15.234 0C18.125 0 20 1.875 20 4.766v10.468C20 18.125 18.125 20 15.234 20H4.766C1.875 20 0 18.125 0 15.234V4.766C0 1.875 1.875 0 4.766 0h10.468z"></path><path d="M0 0h20v20H0z"></path><path fill="#FFF" d="M10.313 10.352c0-.012-.004-1.036-.328-1.055h-2.26a48.42 48.42 0 0 0 .095-3.164h2.21c0-.003.084-.982-.37-.977H5.837c.15-.563.34-1.15.566-1.758 0 0-1.039 0-1.393.937-.146.386-.57 1.87-1.325 3.388.254-.028 1.095-.05 1.59-.963.091-.255.109-.289.222-.627h1.246c0 .452-.051 2.897-.072 3.164H4.414c-.507.019-.672 1.043-.672 1.055h2.832c-.19 2.15-1.209 3.972-3.058 5.413.884.253 1.766-.04 2.202-.43 0 0 .992-.906 1.536-3.004l2.33 2.817s.342-1.166-.053-1.734c-.328-.387-1.212-1.434-1.589-1.814l-.631.504a7.686 7.686 0 0 0 .34-1.752h2.662zM10.898 5v9.441h1.015l.334 1.145 1.82-1.144h2.144V5h-5.313zm4.258 8.398h-1.214l-1.212.782-.22-.782h-.518V6.016h3.164v7.382z"></path></g></svg>
+							<p>下载知乎App</p>
+						
+						</div>
+						
+					</div>
+				</div>
+				
+				<div class="t-center">
+					<div class="d-inline-block end">
+						<p>知乎专栏·圆桌·发现·移动应用·联系我们·来知乎工作·注册机构号</p>
+						<p>© 2020 知乎京· ICP 证·110745 号·京 ICP 备 13052560 号 - 1·京公网安备 11010802010035 号·出版物经营许可证</p>
+						<p>侵权举报·网上有害信息举报专区·儿童色情信息举报专区·违法和不良信息举报：010-82716601</p>
+					</div>
+				</div>
+				
+			
 
-			<!-- <div>
-				<div class="message" v-if="pop">
-					<h1>{{ info }}</h1>
-				</div>
-		
-				<div class="msg-box" v-if="popPlus">
-					<p>{{ info }}</p>
-				</div>
-			</div> -->
+			
 		</div>
 	</div>
 </template>
@@ -55,164 +119,49 @@
 export default {
 	data() {
 		return {
-			userDto: {
+		
 				mobile: '',
 				password: '',
-				code: ''
-			},
-			codeDisabled: true,
-			info: '',
 			isActive: true,
 			show: true,
-			selected: 0,
-			status: '',
-			yzmDisabled: false,
+			selected: 0,			
 			user: null,
-			token: ''
+
 		};
 	},
 
 	created() {
-		this.axios.get('http://localhost:8080/api/code', { responseType: 'blob' }).then(res => {
-			// console.log(res);
-			var img = this.$refs.codeImg;
-			let url = window.URL.createObjectURL(res.data);
-			img.src = url;
-			console.log(res.headers);
-			//取得后台通过响应头返回的sessionId的值
-			this.token = res.headers['access-token'];
-			console.log(this.token);
-		});
-		// this.$router.go(0)
-	},
-	computed: {
-		// 解决403图片缓存问题
-		getImages(_url) {
-			if (_url) {
-				let _u = _url.substring(8);
-				return 'https://images.weserv.nl/?url=' + _u;
-			}
-		}
+		
 	},
 
 	methods: {
-		signIn(userDto) {
-			this.axios({
-				method: 'post',
-				url: 'http://localhost:8080/api/user/sign-in',
-				data: JSON.stringify(this.userDto),
-				headers: {
-					'Access-Token': this.token //将token放在请求头带到后端
-				}
-			}).then(res => {
-				console.log(res.data);
-				if (res.data.msg === '成功') {
-					alert('登录成功');
-					localStorage.setItem('user', JSON.stringify(res.data.data));
-					this.$router.push('/');
-					this.$router.go(0);
-				} else {
-					alert(res.data.msg);
-					this.userDto.code = '';
-				}
-			});
-		},
-		signUp(userDto) {
-			this.axios({
-				method: 'post',
-				url: 'http://localhost:8080/api/user/sign-up',
-				data: JSON.stringify(this.userDto)
-			}).then(res => {
-				console.log(res.data.msg);
-				if (res.data.msg === '成功') {
-					alert('注册成功');
-					this.$router.push('/');
-				} else {
-					alert(res.data.msg);
-					console.log(res.data.msg);
-				}
-			});
-		},
-
-		refresh() {
-			this.axios.get('http://localhost:8080/api/code', { responseType: 'blob' }).then(res => {
-				console.log(res);
-				var img = this.$refs.codeImg;
-				let url = window.URL.createObjectURL(res.data);
-				img.src = url;
-			});
-		},
 		changeTab: function() {
-			this.isActive = !this.isActive;
-			this.selected = this.selected == 0 ? 1 : 0;
-		},
-
-		checkLength() {
-			if (Number(this.userDto.mobile) && this.userDto.mobile.length == 11) {
-				this.codeDisabled = false;
-				return true;
-			} else {
-				return false;
-			}
-		},
-
-		clear() {
-			// alert("我进入到了clear()方法中");
-			this.userDto.nickname = '';
-			this.userDto.mobile = '';
-			this.userDto.password = '';
-			this.pwd2 = '';
-			this.status = '';
-			this.codeDisabled = true;
-		},
-
-		register(userDto) {
-			if (this.userDto.nickname == '') {
-				this.info = '用户名不能为空';
-				this.showMsg();
-				return;
-			}
-			if (this.userDto.password == '' || this.pwd2 == '') {
-				this.info = '密码不能为空';
-				this.showMsg();
-				return;
-			}
-			if (this.userDto.mobile == '') {
-				this.info = '手机号不能为空';
-				this.showMsg();
-				return;
-			}
-			if (!/^1[34578]\d{9}$/.test(this.userDto.mobile)) {
-				this.info = '手机号码格式错误';
-				this.userDto.mobile = '';
-				return;
-			}
-			// 注册信息完全符合要求则进行下面注册操作
-			this.axios.post('http://localhost:8080/api/user/sign-up', JSON.stringify(this.userDto)).then(response => {
-				if (response.data.msg == '成功') {
-					alert('注册成功');
-					// 将后台的用户信息存入本地存储
-					localStorage.user = JSON.stringify(response.data.data);
-					// 路由跳转
-					this.$router.push('/');
+					this.isActive = !this.isActive;
+					this.selected = this.selected == 0 ? 1 : 0;
+				},
+				signIn(mobile, password) {
+					this.axios({
+						method: 'post',
+						url: 'http://localhost:8080/api/user/login',
+						params: {
+							mobile: this.mobile,
+							password: this.password
+						}
+					}).then(res => {
+						if (res.data.msg === '成功') {
+							alert('登录成功');
+							localStorage.setItem('user', JSON.stringify(res.data.data));
+							this.$router.push('/home/recommoned');
+						} else {
+							alert(res.data.msg);
+							alert('密码错误');
+						}
+					});
 				}
-			});
-			this.axios({
-				method: 'post',
-				url: 'http://localhost:8080/api/user/sign-up',
-				data: JSON.stringify(this.userDto),
-				headers: {
-					'Access-Token': this.token
-				}
-			}).then(res => {
-				if (res.data.msg === '成功') {
-					alert('注册成功');
-					localStorage.setItem('user', JSON.stringify(res.data.data));
-					this.$router.push('/');
-				}
-			});
-		}
+
 	}
+	
+		
 };
 </script>
 
@@ -221,11 +170,17 @@ export default {
 	background-image: url(../assets/login1.png);
 
 	width: 100%;
-	height: 600px;
+	
 	background-size: 100% 155%;
 	background-repeat: no-repeat;
 	background-attachment: fixed;
 	padding: 30px;
+}
+.active {
+	color: black;
+	font-weight: 700;
+	padding-bottom: 20px;
+	border-bottom: 3px solid #0084ff;
 }
 .zi {
 	width: 128px;
@@ -233,20 +188,176 @@ export default {
 	margin-bottom: 24px;
 }
 .login-box {
-	width: 450px;
+	width: 400px;
 	height: auto;
 	background-color: rgb(255, 255, 255);
 	padding: 20px;
-	border-radius: 5px;
+	border-top-left-radius: 5px;
+	border-top-right-radius: 5px;
 }
 .tab {
 	height: 60px;
 	
 }
+.line{
+	margin-top: 10px;
+	height: 2px;
+	width: 100%;
+	background-color: rgb(235, 235, 235);
+}
 .tab-item {
 	margin-right: 20px;
-}
-.tab-box{
+	height: 40px;
 	
+}
+.tab-box {
+	
+	padding-top: 10px;
+	padding-bottom: 10px;
+	
+}
+.drop-down-box {
+	height: 30px;
+	
+}
+.drop-down {
+	width: 30%;
+	border: none;
+	border-right: 1px solid rgb(235, 235, 235);
+	height: 30px;
+	color: rgb(133, 144, 166);
+	font-size: 15px;
+}
+.drop-down:hover {
+	color: rgb(118, 131, 155);
+}
+form select {
+	-webkit-appearance: none;
+}
+.pho {
+	width: 70%;
+}
+.num {
+	border: none;
+	width: 90%;
+	
+}
+.paw {
+	width: 100%;
+	border-bottom: 1px solid  rgb(235, 235, 235);
+height: 60px;
+
+	background-color: white;
+}
+.paw-b {
+	width: 70%;
+	height: 40px;
+	position: relative;
+	top: 10px;
+	border: none;
+}
+.paw-note{
+	position: relative;
+	top: 20px;
+	font-size: 15px;
+	color: rgb(23,81,153);
+	
+}
+.paw-note:hover{
+	color: rgb(118,131,155);
+}
+.note-2{
+	position: relative;
+	top: 20px;
+	color: rgb(118,131,155);
+}
+.note-2:hover{
+	color: rgb(128,128,128);
+}
+.login-buttom{
+	margin-top: 30px;
+}
+.login-btn{
+	width: 100%;
+	height: 40px;
+	border-radius: 5px;
+	border: none;
+	background-color: rgb(0,132,255);
+	color: white;
+}
+.login-btn:hover{
+	background-color: rgb(0,119,230);
+}
+.grey-box{
+	height: 60px;
+	width: 400px;
+	background-color: rgb(246,246,246);
+	padding-right: 20px;
+	padding-left:20px ;
+	padding-top: 10px;
+	padding-bottom: 10px;
+	border-bottom-left-radius: 5px;
+	border-bottom-right-radius: 5px;
+}
+.grey-a{
+	font-size: 13px;
+	color: rgb(128,128,128);
+	width: 80%;
+}
+.grey-b{
+	font-size: 13px;
+	color: rgb(23,81,153);
+	
+}
+.white-box{
+	height: 60px;
+	line-height: 60px;
+	width: 400px;
+	background-color: white;margin-top: 30px;
+	border-radius: 5px;
+}
+.white-box-2{
+	height: 40px;
+	line-height: 40px;
+	width: 400px;
+	background-color: white;margin-top: 30px;
+	border-radius: 5px;
+}
+.whiter-a{
+	color: rgb(133,144,166);
+	width: 40%;
+}
+.whiter-b{
+	color: rgb(133,144,166);
+	width: 75%;
+	height: auto;
+	justify-content: space-around;
+}
+.num-2{
+	border: none;
+	border-bottom: 1px solid rgb(235,235,235);
+	width: 100%;
+	height: 60px;
+	
+}
+.forget{
+	margin-top: 10px;
+}
+.forget-a{
+	color: rgb(23,81,153);
+}
+.forget-b{
+	color: rgb(133,144,166);
+}
+.forget-a:hover, .forget-b:hover{
+	color: rgb(128,128,128);
+}
+.end{
+	color: white;
+	font-size: 12px;
+	width: 80%;
+	margin-top: 30px;
+	line-height: 25px;
+	letter-spacing:2px;
 }
 </style>
--- src/views/Nav.vue
+++ src/views/Nav.vue
@@ -39,7 +39,7 @@
 										<div class="login">
 											<svg class="grey zi" fill="currentColor" viewBox="0 0 25 25" width="25" height="25"><path d="M4.523 15.076l.804-6.757a6.753 6.753 0 0 1 4.945-5.7 1.823 1.823 0 0 1 3.623 0 6.753 6.753 0 0 1 4.945 5.7l.804 6.757a2.293 2.293 0 0 0 1.712 2.108 1.093 1.093 0 0 1-.297 2.15H3.108a1.093 1.093 0 0 1-.297-2.15 2.293 2.293 0 0 0 1.712-2.108zM12.083 23a2.758 2.758 0 0 1-2.753-2.509.229.229 0 0 1 .232-.24h5.043a.229.229 0 0 1 .232.24 2.759 2.759 0 0 1-2.753 2.51z"></path></svg>
 											<svg class="grey zi" fill="currentColor" viewBox="0 0 25 25" width="25" height="25"><path d="M11 2c5.571 0 9 4.335 9 8 0 6-6.475 9.764-11.481 8.022-.315-.07-.379-.124-.78.078-1.455.54-2.413.921-3.525 1.122-.483.087-.916-.25-.588-.581 0 0 .677-.417.842-1.904.064-.351-.14-.879-.454-1.171A8.833 8.833 0 0 1 2 10c0-3.87 3.394-8 9-8zm10.14 9.628c.758.988.86 2.009.86 3.15 0 1.195-.619 3.11-1.368 3.938-.209.23-.354.467-.308.722.12 1.073.614 1.501.614 1.501.237.239-.188.562-.537.5-.803-.146-1.495-.42-2.546-.811-.29-.146-.336-.106-.563-.057-2.043.711-4.398.475-6.083-.927 5.965-.524 8.727-3.03 9.93-8.016z" fill-rule="evenodd"></path></svg>
-											<img :src="user.avatar" v-if="token" width="30" height="30" class="zi" @click="show = !show" />
+											<img :src="user.avatarUrl" v-if="this.user !==null" width="30" height="30" class="zi" @click="show = !show" />
 																	<span v-else class="link"><router-link to="/login">登录</router-link></span>
 																	
 																	<transition name="fade">
@@ -79,7 +79,8 @@
 																						fill-rule="evenodd"
 																					></path>
 																				</svg>
-																				<a @click.prevent.stop="logout($event)" href="#" class="grey">退出</a>
+																				
+																				<router-link to="/login" @click.prevent.stop="logout($event)" href="#" class="grey">退出</router-link>
 																			</li>
 																		</ul>
 																	</transition> 
@@ -123,8 +124,7 @@ export default {
 	name: 'banner',
 	data() {
 		return {
-			token: this.$store.state.token,
-			user: this.$store.state.user,
+			user: JSON.parse(localStorage.getItem('user')),
 			fixed: true,
 			show: false,
 			popUp: false
@@ -161,9 +161,9 @@ export default {
 			}
 		},
 		logout(event) {
-			this.token = '';
+			this.user = null;
 			this.show = false;
-			this.$store.commit('setToken', '');
+			localStorage.clear();
 		}
 	}
 };
